0de1dad1c93723ccaedce3942fefa5f2fb96e3c5
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-03-31 10:21:32 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//console.log(app);
+
+app.get('/',(req,res) => {
+    res.send('Olá mundo!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!');
+});
